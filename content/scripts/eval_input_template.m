bb0b17e081819894eb65158e672aebc0b8cb3fcf
Sergie7 <59622343+Sergie7@users.noreply.github.com>
2020-01-07 15:25:15 -0800

    Made template more general for road profile and updated comments

--- content/scripts/eval_input_template.m
+++ content/scripts/eval_input_template.m
@@ -5,12 +5,12 @@
       %
       % Inputs:
       %   t - A scalar value of time, size 1x1.
-      %   x - State vector at time t, size mx1 were m is the number of states.
+      %   x - State vector at time t, size 3x1 where the states are mass momentum, spring deflection and road height.
       %   p - Constant parameter structure with p items where p is the number of
       %       parameters.
       % Outputs:
       %   r - Input vector at time t, size ox1 where o is the number of
-      %       inputs.
+      %       inputs.  In this case, 1.
 
       % NOTE : x is not needed in this case
       
@@ -18,15 +18,6 @@
       amp = p.amp;
       
       % define a triangular road bump input
-      if t < .5
-      vin = ;
-      elseif t>=.5 & t<1
-      vin = ;
-      elseif t>=1 & t<1.5
-      vin = ;
-      else
-      vin = ;
-      end
   
       % pack the inputs into an 1x1 vector
       r = [vin];
