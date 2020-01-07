a611d16843e8b74fd5d0261c4b0251bb766dc6d0
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-07 23:11:00 +0000

    Update docs after building Travis build 192 of moorepants/eme171
    The docs were built from the branch 'master' against the commit
    a87b58a96def5f4fd2ad9f9b3aae87aa89127cba.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/moorepants/eme171/jobs/633998823.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --build-tags --built-docs output/ .

---
+++ scripts/eval_output_with_state_derivatives_template.m
@@ -0,0 +1,46 @@
+function z = eval_output_with_state_derivatives_template(t, xdot, x, r, p)
+    % EVAL_OUTPUT_WITH_STATE_DERIVATIVES_TEMPLATE - Returns the output vector at the
+    % specified time.
+    %
+    % Syntax: z = eval_output_with_state_derivatives(t, xdot, x, r, p)
+    %
+    % Inputs:
+    %   t - Scalar value of time, size 1x1.
+    %   xdot - State derivative vector as time t, size mx1 where m is the
+    %          number of states
+    %   x - State vector at time t, size mx1 where m is the number of
+    %       states. 
+    %   r - Input vector at time t, size ox1 were o is the number of inputs.
+    %   p - Constant parameter structure with p items where p is the number
+    %       of parameters.
+    % Outputs:
+    %   z - Output vector at time t, size qx1.
+
+    % unpack all the vectors
+     = xdot(1);
+     = xdot(2);
+     = xdot(3);
+      
+     = x(1);
+     = x(2);
+     = x(3);
+
+    % unpack the parameters
+    m = p.m;
+    k = p.k;
+    b = p.b;
+
+    % calculate the force applied to the spring
+     = ;
+
+    % calculate the force applied to the damper
+     = ;
+       
+    % calculate the vertical acceleration of the mass 
+     = ;
+      
+    % pack the outputs into a qx1 vector
+    z = [ ; ; ];
+
+  end
+
---
+++ scripts/eval_rhs_with_input_template.m
@@ -0,0 +1,45 @@
+function xdot = eval_rhs_with_input(t, x, w, p)
+       % EVAL_RHS_WITH_INPUT - Returns the time derivative of the states, i.e.
+       % evaluates the right hand side of the explicit ordinary differential
+       % equations.
+       %
+       % Syntax: xdot = eval_rhs_with_input(t, x, w, p)
+       %
+       % Inputs:
+       %   t - Scalar value of time, size 1x1.
+       %   x - State vector at time t, size mx1 where m is the number of
+       %       states.
+       %   w - Anonymous function, w(t, x, p), that returns the input vector
+       %       at time t, size ox1 were o is the number of inputs.
+       %   p - Constant parameter structure with p items where p is the number
+       %       of parameters.
+       % Outputs:
+       %   xdot - Time derivative of the states at time t, size mx1.
+
+       % unpack the states into useful variable names
+        = x(1);
+        = x(2);
+        = x(3);
+
+       % evaluate the input function
+       r = w(t, x, p);
+
+       % unpack the inputs into useful variable names
+       vin = r;
+       % if more than one input, then tau = r(1);, ...
+
+       % unpack the parameters into useful variable names
+       m = p.m;
+       k = p.k;
+       b = p.b;
+
+       % calculate the state derivatives
+        = ;
+        = ;
+        = ;
+
+       % pack the state derivatives into an mx1 vector
+       xdot = [ ; ; ];
+
+  end
+
