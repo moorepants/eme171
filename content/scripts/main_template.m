f67ccf5004164f6ba5363255bf42db5c1af412a9
Jason K. Moore <moorepants@gmail.com>
2020-01-07 22:17:26 -0800

    Improved the main script.

--- content/scripts/main_template.m
+++ content/scripts/main_template.m
@@ -1,20 +1,27 @@
-% integrate the ODEs as defined above
+% define the initial conditions
 x0 = [ ; ; ];
+
+% define a set of time values
 ts = ;
-p.m = ;
-p.k = ;
-p.b = ;
-p.amp = ;
-f_anon = @(t, x) eval_rhs_with_input(t, x, @eval_input, p);
+
+% set the numerical values for the constants
+c.M = ;
+c.K = ;
+c.B = ;
+c.A = ;
+
+% integrate the differential equations
+f_anon = @(t, x) eval_rhs_with_input(t, x, @eval_input, c);
 [ts, xs] = ode45(f_anon, ts, x0);
 
-% to compute the outputs at each time, loop through time evaluating f(t, x,
-% r, p) and then h(t, x', x, r, p)
+% evaluate the outputs
 zs = zeros(length(ts), 3);  % place to store outputs
 xdot = zeros(length(ts), 3);  % place to store outputs
 for i=1:length(ts)
     % calculate x' at the given time
-    xdot = eval_rhs_with_input(ts(i), xs(i, :), @eval_input, p);
+    xdot = eval_rhs_with_input_template(ts(i), xs(i, :), @eval_input, c);
     % calculate the outputs that depend on x' and store them
-    zs(i, :) = eval_output_with_state_derivatives(ts(i), xdot, xs(i, :), @eval_input, p);
+    zs(i, :) = eval_output_with_state_derivatives_template(ts(i), xdot, xs(i, :), @eval_input_template, c);
 end
+
+% write your code to make the plots below
