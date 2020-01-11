76f8b2606a9d31da5809ac7a5cd5223da69dfad6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-11 18:57:17 +0000

    Update docs after building Travis build 220 of moorepants/eme171
    The docs were built from the branch 'master' against the commit
    730a798b8b241198b8c0852763b938c6e791ad1e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/moorepants/eme171/jobs/635750338.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --build-tags --built-docs output/ .

--- scripts/best-practices/eval_rhs_with_input.m
+++ scripts/best-practices/eval_rhs_with_input.m
@@ -1,42 +1,41 @@
 function xdot = eval_rhs_with_input(t, x, w, p)
-     % EVAL_RHS_WITH_INPUT - Returns the time derivative of the states, i.e.
-     % evaluates the right hand side of the explicit ordinary differential
-     % equations.
-     %
-     % Syntax: xdot = eval_rhs_with_input(t, x, w, p)
-     %
-     % Inputs:
-     %   t - Scalar value of time, size 1x1.
-     %   x - State vector at time t, size mx1 where m is the number of
-     %       states.
-     %   w - Anonymous function, w(t, x, p), that returns the input vector
-     %       at time t, size ox1 were o is the number of inputs.
-     %   p - Constant parameter structure with p items where p is the number
-     %       of parameters.
-     % Outputs:
-     %   xdot - Time derivative of the states at time t, size mx1.
+% EVAL_RHS_WITH_INPUT - Returns the time derivative of the states, i.e.
+% evaluates the right hand side of the explicit ordinary differential
+% equations.
+%
+% Syntax: xdot = eval_rhs_with_input(t, x, w, p)
+%
+% Inputs:
+%   t - Scalar value of time, size 1x1.
+%   x - State vector at time t, size mx1 where m is the number of states.
+%   w - Anonymous function, w(t, x, p), that returns the input vector at
+%       time t, size ox1 were o is the number of inputs.
+%   p - Constant parameter structure with p items where p is the number of
+%       parameters.
+% Outputs:
+%   xdot - Time derivative of the states at time t, size mx1.
 
-     % unpack the states into useful variable names
-     theta = x(1);
-     omega = x(2);
+% unpack the states into useful variable names
+theta = x(1);
+omega = x(2);
 
-     % evaluate the input function
-     r = w(t, x, p);
+% evaluate the input function
+r = w(t, x, p);
 
-     % unpack the inputs into useful variable names
-     tau = r;
-     % if more than one input, then tau = r(1);, ...
+% unpack the inputs into useful variable names
+tau = r;
+% if more than one input, then tau = r(1);, ...
 
-     % unpack the parameters into useful variable names
-     m = p.m;
-     l = p.l;
-     g = p.g;
+% unpack the parameters into useful variable names
+m = p.m;
+l = p.l;
+g = p.g;
 
-     % calculate the state derivatives
-     thetadot = omega;
-     omegadot = (-m*g*l*sin(theta) + tau)/(m*l^2);
+% calculate the state derivatives
+thetadot = omega;
+omegadot = (-m*g*l*sin(theta) + tau)/(m*l^2);
 
-     % pack the state derivatives into an mx1 vector
-     xdot = [thetadot; omegadot];
+% pack the state derivatives into an mx1 vector
+xdot = [thetadot; omegadot];
 
 end
